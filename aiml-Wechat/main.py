07346a96b2901081c1fc6a39de51bc58d8dd46b1
LexLuc <lexluc.lei@gmail.com>
2018-10-13 21:09:38 +1100

    add local debug mode on main.py

--- aiml-Wechat/main.py
+++ aiml-Wechat/main.py
@@ -1,17 +1,18 @@
 import aiml
 
 PATH_KNOWLEDGE_BASE = 'FAQ_FULL.aiml'
+LOCAL_DEBUG = True
 
 #Create the kernel and learn AIML files
 kernel = aiml.Kernel()
 kernel.learn(PATH_KNOWLEDGE_BASE)
 kernel.respond("Hi")
 
-
-#while True:
-#    Question_Input = input("")
-#    Answers = kernel.respond(input("Input:"))
-#    print(kernel.respond(input("Input:")))
+if LOCAL_DEBUG is True:
+    while True:
+        Question_Input = input("")
+        Answers = kernel.respond(input("Input:"))
+        print(kernel.respond(input("Input:")))
 
 def FAQ_Answers(message):
     Answers = kernel.respond(message)
