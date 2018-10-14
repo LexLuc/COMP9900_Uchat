d9d8e030d55810f2a391800860aa2a6ff7e07ab2
MingkaiMa-z5135897 <41951401+MingkaiMa-z5135897@users.noreply.github.com>
2018-10-14 15:41:05 +1100

    Add files via upload

--- testcases/run.py
+++ testcases/run.py
@@ -6,4 +6,11 @@ kernel.learn("std-startup.xml")
 kernel.respond("load aiml b")
 
 while True:
-    print(kernel.respond(input("Input:")))
+
+    input_text = input('Input: ')
+    input_text = input_text.replace('?', ' qeustion mark ')
+    input_text = input_text.replace('.', ' exclamation point ')
+    input_text = input_text.replace('.', ' period point ')
+    input_text = input_text.replace('\'', '_')
+    
+    print(kernel.respond(input_text))
