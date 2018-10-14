49697fc6a5fe2ca590d4f02e81702a16684d5ff4
LexLuc <lexluc.lei@gmail.com>
2018-10-14 14:39:31 +1100

    update dailychat api backend support

---
+++ aiml-daily_chat/dailychat_backendapi.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+import aiml
+import os
+
+
+app = Flask(__name__)
+
+
+@app.route('/api/dailychat', methods=['POST'])
+def my_form_post():
+    req_content = request.get_json()
+    question = req_content['question']
+    ans_content = kernel.respond(question)
+    
+    return jsonify(whatIsQuestion=ans_content)
+
+if __name__ == '__main__':
+    kernel = aiml.Kernel()
+    kernel.learn(os.sep.join(['alice', '*.aiml']))
+    
+    app.run(host = "127.0.0.1", port=8080)
